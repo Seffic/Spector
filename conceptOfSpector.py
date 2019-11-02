989290a7bb75f4868ad9753463821beb705a6384
Seffic <57186071+Seffic@users.noreply.github.com>
2019-11-02 18:30:33 -0500

    Add files via upload

---
+++ conceptOfSpector.py
@@ -0,0 +1,24 @@
+
+
+#program 1 on drone
+#setup to moniter mode
+#pick a network and connect to it
+#hostapd. start new network and set SSID name to coppied name.
+while(true):
+    #hcpd. to hand out ip address
+    #dns cheif. sees all webtrafic
+    #sends to program 2
+    #check for command and follows with command
+
+
+
+#program 2 on local
+while(true):
+    #get input from other pie
+    #display  activity on screen browzer history GUI
+
+
+#intturpt with GUI input
+    #send commands to other pie
+    #possible commands are renameSSID(string), end()
+
