ae65b989c58911155e0ba650cbc44c28b2afcd15
Seffic <57186071+Seffic@users.noreply.github.com>
2019-11-07 23:24:50 -0600

    Add files via upload

--- HomeBase.py
+++ HomeBase.py
@@ -1,108 +1,96 @@
-
-#while(true):
-    #get input from other pie
-    #display  activity on screen browzer history GUI
-
-
-#intturpt with GUI input
-    #send commands to other pie
-    #possible commands are renameSSID(string), end()
-
+###############################################################
+#Names: Jace Ziegler, Cole Edwards, Stuart Redfearn
+#Class: CSC 132-02
+#Date: 11/6/2019
+#Disc: program to opperate recon drone
 ###############################################################
 from Tkinter import *
+import paramiko
+import time
 
 class MainGUI(Frame):
         def __init__(self, master):
                 Frame.__init__(self, master, bg="white")
-
+                
+        # call all needed functions to start
         def start(self):
                 self.setupGUI()
                 self.setResponse("Welcome to Spector.")
-                self.setImage()
                 
-                
-
         def setupGUI(self):
                 # pack the GUI
                 self.pack(fill=BOTH, expand=1)
-                
-                MainGUI.player_input = Entry(self, bg="white")
-                MainGUI.player_input.bind("<Return>", self.process)
-                MainGUI.player_input.pack(side=BOTTOM, fill=X)
-                MainGUI.player_input.focus()
 
-                # setup the watch frame of the GUI
-                # first, the frame in which the text will be placed
-                moniter_frame = Frame(self, width=WIDTH/2)
-                #the widget is a Tkinter Text
-                # disable it by default
-                # don't let the widget control the frame's size
-                img = None
-                MainGUI.image = Label(self, width=WIDTH / 2, image=img)
-                MainGUI.image.image = img
-                MainGUI.image.pack(side=LEFT, fill=Y)
-                MainGUI.image.pack_propagate(False)
+               # setup entry from to get entry from person 
+                MainGUI.user_input = Entry(self, bg="white")
+                MainGUI.user_input.bind("<Return>", self.process)
+                MainGUI.user_input.pack(side=BOTTOM, fill=X)
+                MainGUI.user_input.focus()
+
+
                 # setup the text to the right of the GUI
-                # first, the frame in which the text will be placed
+                # the frame in which the text will be placed
                 text_frame = Frame(self, width=WIDTH)
-                #the widget is a Tkinter Text
+                # the widget is a Tkinter Text
                 # disable it by default
                 # don't let the widget control the frame's size
-                MainGUI.text = Text(text_frame, bg="lightgrey", state=DISABLED)
+                MainGUI.text = Text(text_frame, bg="grey", state=DISABLED)
                 MainGUI.text.pack(fill=Y, expand=1)
                 text_frame.pack(side=LEFT, fill=Y)
                 text_frame.pack_propagate(False)
 
 
-
+        #sets respons on the text 
         def setResponse(self, response):
+                #make text editable
                  MainGUI.text.config(state=NORMAL)
+                 #delete prior text
                  MainGUI.text.delete("1.0", END)
-
+                 #insert text
                  MainGUI.text.insert(END,response)
-                
+                 #reset disabled state
                  MainGUI.text.config(state=DISABLED)
-
-        def setImage(self):
-                MainGUI.img = PhotoImage(file="skull.gif")
-                # display the image on the left of the GUI
-                MainGUI.image.config(image=MainGUI.img)
-                MainGUI.image.image = MainGUI.img
                 
 
         def process(self, event):
-                # grab the player's input from the input at the bottom of
-                # the GUI
-                command = MainGUI.player_input.get()
-                # set the user's input to lowercase to make it easier to
-                # compare the verb and noun to known values
+                #grab the input from the input at the bottom of the GUI
+                command = MainGUI.user_input.get()
+
+                #set the user's input to lowercase to make it easier to
                 command= command.lower()
-                # set a default response
+
+                #set a default response
                 response = "I don't understand."
                 
-                # exit the game if the player wants to leave (supports quit,
-                # exit, and bye)
+                 #exit the game if the user wants to leave (supports quit, exit, and bye)
                 if (command == "quit" or command == "exit" or command == "bye" or command == "sionara!"):
                     exit(0)
 
-                elif(command == "ssh"):
-                        response = "started ssh"
-
-                elif(command == "scan"):
-                        #sudo airodump-ng wlanX - gives list of mac addresses and channels
+                # puts wlan1 into moniter mode 
+                elif(command == "moniter"):
+                        # sudo airodump-ng wlanX - gives list of mac addresses and channels
+                        stdin, stout, stderr = ssh.exec_command("iwconfig wlan1 mode monitor")
                         response = "scan started"
 
-                elif(command == "twin"):
-                        #sudo airbase-ng -a MAC ADDRESS --essid "Wifi Name" -c "channel #" wlanX:
-                        response = "started twin"
+                # create the graph
+                elif(command == "graph"):
+                        # tell user to wait
+                        self.setResponse("Wait")
+                        MainGUI.user_input.delete(0, END)
+                        # start arodump to find info
+                        stdin, stout, stderr = ssh.exec_command('airodump-ng wlan1 -w "/root/Graphs"')
+                        # wait 5 sec till controll + c
+                        time.sleep(5)
+                        stdin, stout, stderr = ssh.exec_command(chr(3))
+                        # make the graph with info from arodump
+                        time.sleep(3)
+                        stdin, stout, stderr = ssh.exec_command("airgraph-ng -i '/root/Graphs-01.csv' -o '/root/Graphs-01' -g CAPR")
+                        stdin, stout, stderr = ssh.exec_command(chr(3))
+                        response = "graphed"
 
-                elif(command == "renamessid"):
-                        # send to rename id
-                        response = "renamed SSID"
-
-                
+                        
                 self.setResponse(response)
-                MainGUI.player_input.delete(0, END)
+                MainGUI.user_input.delete(0, END)
                 
                
                 
@@ -111,12 +99,22 @@ class MainGUI(Frame):
 # the default size of the GUI is 800x600
 WIDTH = 800
 HEIGHT = 600
+# credintals
+password = "hd123"
+host = "10.0.0.18"
+users = "root"
+
+# start ssh with paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname = host, username = users, password = password)
 
 window = Tk()
 window.geometry("{}x{}".format(WIDTH, HEIGHT))
 window.title("Spector")
 # create the game as a Tkinter Frame inside the window
 s = MainGUI(window)
+# call needed functions
 s.start()
 # wait for the window to close
 window.mainloop()
